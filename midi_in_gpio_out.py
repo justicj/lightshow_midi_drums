36a1b57260eb5f45c285b5259cca04ba4b1cfc76
justicj <jayjustice@gmail.com>
2019-12-02 22:04:11 -0800

    it works!

--- midi_in_gpio_out.py
+++ midi_in_gpio_out.py
@@ -8,7 +8,7 @@ pygame.midi.init()
 
 # variables
 apiaddress = '192.168.1.219:8080'
-apitimeout = 0.001
+apitimeout = 0.2
 gpiodelay = 0.1
 snare = [201, 81, 0, 0]
 tom = [153, 22, 0, 0]
@@ -28,6 +28,7 @@ inp = pygame.midi.Input(1)
 def sendtoapi(url, gpio):
     urlon = 'http://' + url + '/' + str(gpio) + '/true'
     urloff = 'http://' + url + '/' + str(gpio) + '/false'
+    print(urlon, urloff)
     try:
         r = requests.get(urlon, timeout=apitimeout)
     except requests.exceptions.ReadTimeout:
@@ -56,28 +57,28 @@ def listentrigger():
             midi_out = inp.read(1000)
             # print(midi_out)
             if any(snare in x for x in midi_out):
-                sendtoapi(apiaddress, 1)
+                sendtoapi(apiaddress, 4)
                 print('Snare')
             elif any(tom in x for x in midi_out):
-                sendtoapi(apiaddress, 2)
+                sendtoapi(apiaddress, 17)
                 print('Tom')
             elif any(tom_medium in x for x in midi_out):
-                sendtoapi(apiaddress, 3)
+                sendtoapi(apiaddress, 18)
                 print('Tom Medium')
             elif any(hi_hat in x for x in midi_out):
-                sendtoapi(apiaddress, 4)
+                sendtoapi(apiaddress, 22)
                 print('Hit-Hat')
             elif any(tom_floor in x for x in midi_out):
-                sendtoapi(apiaddress, 5)
+                sendtoapi(apiaddress, 23)
                 print('Tom Floor')
             elif any(crash in x for x in midi_out):
-                sendtoapi(apiaddress, 6)
+                sendtoapi(apiaddress, 24)
                 print('Crash')
             elif any(ride in x for x in midi_out):
-                sendtoapi(apiaddress, 7)
+                sendtoapi(apiaddress, 25)
                 print('Ride')
             elif any(bass in x for x in midi_out):
-                sendtoapi(apiaddress, 8)
+                sendtoapi(apiaddress, 27)
                 print('Bass')
         # wait 10ms - this is arbitrary, but wait(0) still resulted
         # in 100% cpu utilization
