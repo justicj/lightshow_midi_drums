1266be09bb47bf5bc59e682c7ae8f3cab3ea1e1d
justicj <jayjustice@gmail.com>
2019-12-02 21:32:02 -0800

    catching errors on requests.  Still need to test against real api endpoint

--- midi_in_gpio_out.py
+++ midi_in_gpio_out.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
-import sys, pygame, pygame.midi
+import sys, pygame, pygame.midi, requests
+from time import sleep
 
 # set up pygame
 pygame.init()
@@ -14,7 +15,7 @@ for x in range( 0, pygame.midi.get_count() ):
 (b'MMSystem', b'Microsoft GS Wavetable Synth', 0, 1, 0)
 (b'MMSystem', b'Alesis DM6', 0, 1, 0)
 '''
-
+apiaddress = '192.168.1.219:8080'
 snare = [201, 81, 0, 0]
 tom = [153, 22, 0, 0]
 tom_medium = [153, 23, 0, 0]
@@ -26,30 +27,63 @@ bass = [201, 80, 0, 0]
 # open a specific midi device
 inp = pygame.midi.Input(1)
 
-# run the event loop
-while True:
-    if inp.poll():
-    # no way to find number of messages in queue
-    # so we just specify a high max
-        midi_out = inp.read(1000)
-        # print(midi_out)
-        if any(snare in x for x in midi_out):
-            print('Snare')
-        elif any(tom in x for x in midi_out):
-            print('Tom')
-        elif any(tom_medium in x for x in midi_out):
-            print('Tom Medium')
-        elif any(hi_hat in x for x in midi_out):
-            print('Hit-Hat')
-        elif any(tom_floor in x for x in midi_out):
-            print('Tom Floor')
-        elif any(crash in x for x in midi_out):
-            print('Crash')
-        elif any(ride in x for x in midi_out):
-            print('Ride')
-        elif any(bass in x for x in midi_out):
-            print('Bass')
+def sendtoapi(url, gpio):
+    urlon = 'http://' + url + '/' + str(gpio) + '/true'
+    urloff = 'http://' + url + '/' + str(gpio) + '/false'
+    try:
+        r = requests.get(urlon, timeout=0.001)
+    except requests.exceptions.ReadTimeout:
+        print('Turning on, didnt make it to the api')
+    except requests.exceptions.ConnectionError:
+        print('Turning on, didnt make it to the api')
+    except:
+        raise
+    sleep(0.1)
+    try:
+        r = requests.get(urloff, timeout=0.001)
+    except requests.exceptions.ReadTimeout:
+        print('Turning off, didnt make it to the api')
+    except requests.exceptions.ConnectionError:
+        print('Turning off, didnt make it to the api')
+    except:
+        raise
+    return()
 
-    # wait 10ms - this is arbitrary, but wait(0) still resulted
-    # in 100% cpu utilization
-    pygame.time.wait(10)
+def listentrigger():
+    # run the event loop
+    while True:
+        if inp.poll():
+        # no way to find number of messages in queue
+        # so we just specify a high max
+            midi_out = inp.read(1000)
+            # print(midi_out)
+            if any(snare in x for x in midi_out):
+                sendtoapi(apiaddress, 1)
+                print('Snare')
+            elif any(tom in x for x in midi_out):
+                sendtoapi(apiaddress, 2)
+                print('Tom')
+            elif any(tom_medium in x for x in midi_out):
+                sendtoapi(apiaddress, 3)
+                print('Tom Medium')
+            elif any(hi_hat in x for x in midi_out):
+                sendtoapi(apiaddress, 4)
+                print('Hit-Hat')
+            elif any(tom_floor in x for x in midi_out):
+                sendtoapi(apiaddress, 5)
+                print('Tom Floor')
+            elif any(crash in x for x in midi_out):
+                sendtoapi(apiaddress, 6)
+                print('Crash')
+            elif any(ride in x for x in midi_out):
+                sendtoapi(apiaddress, 7)
+                print('Ride')
+            elif any(bass in x for x in midi_out):
+                sendtoapi(apiaddress, 8)
+                print('Bass')
+        # wait 10ms - this is arbitrary, but wait(0) still resulted
+        # in 100% cpu utilization
+        pygame.time.wait(10)
+    return()
+
+listentrigger()
